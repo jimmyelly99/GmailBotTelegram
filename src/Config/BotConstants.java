8397dc17c51e04bb2b5abf29fc7cbf54eb4ef848
Rostik Kachanov <rostik_99@icloud.com>
2018-05-16 16:20:24 +0300

    Update BotConstants.java

--- src/Config/BotConstants.java
+++ src/Config/BotConstants.java
@@ -1,7 +1,7 @@
 package Config;
 
 public class BotConstants {
-    public static final String TOKEN = "589035906:AAE7C2f_i0rEhb1GvmUyLAlRHvfxpk7N7Ow";
-    public static final String BOT_USERNAME = "Jmail";
+    public static final String TOKEN = "";
+    public static final String BOT_USERNAME = "";
     public static final String DATA_PATH = "data/data.csv";
 }
